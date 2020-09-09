8392b558d6436acfa7085565b06852125ee05f17
Collier <collier.bell20@gmail.com>
2020-09-09 09:47:05 -0500

    Update setup.js

--- commands/admin/setup.js
+++ commands/admin/setup.js
@@ -22,10 +22,6 @@ module.exports = class SetupCommand extends Command {
 	}
 
 	async run(message, args) {
-		let role = args.role;
-		if (args.role.startsWith('<@') && args.role.endsWith('>')) {
-			role = args.role.slice(2, -1);
-			return await message.say(`SERVER ID ${message.guild.id}\nROLE ID: ${role}`);
-		}
+		return await message.say(`SERVER ID ${message.guild.id}\nROLE ID: ${args.role.id}`);
 	}
 };
