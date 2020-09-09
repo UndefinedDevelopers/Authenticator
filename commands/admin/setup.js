c9576ff647762118376abbb577dd13bf63237eaa
Collier <collier.bell20@gmail.com>
2020-09-09 10:24:18 -0500

    ok seirously now please work:anger:

--- commands/admin/setup.js
+++ commands/admin/setup.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 const { Command } = require('discord.js-commando');
 const Keyv = require('keyv');
 const servers = new Keyv(`mysql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:3306/${process.env.DB_NAME}`)
-const { errorLog } = require("../functions/logging");
+const { errorLog } = require(".../functions/logging");
 keyv.on('error', err => errorLog(err));
 
 module.exports = class SetupCommand extends Command {
