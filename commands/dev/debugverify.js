350f9184793170300f0c2442774d1e88fb120ea7
Collier <collier.bell20@gmail.com>
2020-09-12 16:11:12 -0500

    debug verify update;
    debugverify.js was updated; see commit.

--- commands/dev/debugverify.js
+++ commands/dev/debugverify.js
@@ -20,16 +20,16 @@ module.exports = class DebugVerifyCommand extends Command {
         function getRandomString(length) { var s = ""; do { s += Math.random().toString(36).substr(2) } while (s.length < length); return s = s.substr(0, length) }
         let code = getRandomString(6)
         registerFont("PressStart2P.ttf", { family: 'Press Start' })
-        const canvas = createCanvas(100, 50);
+        const canvas = createCanvas(100, 35);
         const ctx = canvas.getContext('2d');
         ctx.fillStyle = "#FF8C00";
         ctx.font = '15px "Press Start"';
-        ctx.fillText(`${code}`, 12, 35);
+        ctx.fillText(`${code.toUpperCase()}`, 12, 35);
         let embed = new MessageEmbed()
             .setTitle(`Welcome to \`${message.guild.name}\``)
-            .setDescription("This server uses verification to keep out robots, Please verify you are not a bot by sending me the code below!")
-            .attachFiles(new MessageAttachment(canvas.toBuffer(), "image.png"))
-            .setImage("attachment://image.png")
+            .setDescription("This server uses verification to keep out robots, Please enter the code below to verify you are not a bot!")
+            .attachFiles(new MessageAttachment(canvas.toBuffer(), "code.png"))
+            .setImage("attachment://code.png")
             .setColor("ORANGE")
             .setFooter(`${message.guild.name} is powered by Authenticator`, `${message.guild.iconURL({ dynamic: true })}`);
         await message.author.send(embed)
