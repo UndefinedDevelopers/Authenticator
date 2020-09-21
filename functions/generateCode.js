c25474f92a80ccf42377a2ca5435a0588e3d2342
Collier <collier.bell20@gmail.com>
2020-09-21 15:42:37 -0500

    lettify

--- functions/generateCode.js
+++ functions/generateCode.js
@@ -1,8 +1,8 @@
 function generateCode(length) {
-    var result           = '';
-    var characters       = 'ABCDEFGHIJKLMNPQRSTUVWXYZ123456789';
-    var charactersLength = characters.length;
-    for ( var i = 0; i < length; i++ ) {
+    let result           = '';
+    let characters       = 'ABCDEFGHIJKLMNPQRSTUVWXYZ123456789';
+    let charactersLength = characters.length;
+    for ( let i = 0; i < length; i++ ) {
        result += characters.charAt(Math.floor(Math.random() * charactersLength));
     }
     return result;
