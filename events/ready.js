0a4bee8828f307ff15dcd904eb5597e152052453
Collier <collier.bell20@gmail.com>
2020-09-23 18:26:17 -0500

    Lowering the cooldown for .setActivity. This will be used for a future feature.

--- events/ready.js
+++ events/ready.js
@@ -3,6 +3,6 @@ module.exports = async (client) => {
     const { infoLog } = require("../functions/logging");
     setInterval(() => {
         client.user.setActivity(`${client.users.cache.size} users | ${client.guilds.cache.size} guilds`, { type: "WATCHING" });
-    }, 120000);
+    }, 60000);
     infoLog(`${client.user.username} connected to discord!`)
 };
